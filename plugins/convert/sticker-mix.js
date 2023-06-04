6c9100d5c0590fa45abb9698f7daf927fab835e4
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-06-05 06:20:27 +0800

    update plugins/convert/sticker-mix.js

--- plugins/convert/sticker-mix.js
+++ plugins/convert/sticker-mix.js
@@ -2,15 +2,14 @@ const fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, args }) => {
     if (!global.db.data.users[m.sender].registered) {
-        if (!m.chat.endsWith('g.us') throw "testing"
-    } else {
+        if (!m.chat.endsWith('g.us')) return conn.reply(m.chat, "testing", m)
+    }
     if (!args[0]) throw 'Contoh penggunaan:\n\n*.emojimix 🤨&😣*'
        	let [emoji1, emoji2] = text.split`&`
     		let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
     		for (let res of anu.results) {
     			conn.sendSticker(m.chat, anu.results, m, { packname: set.pack, author: set.auth, asSticker: true})		
     		}
-    }
 }
 		
 handler.help = ['emojimix']

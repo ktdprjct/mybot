b0182f9d8ba7747466cf55c48701559b0bb4e1ac
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-06-08 10:08:42 +0800

    update plugins/convert/sticker-mix.js

--- plugins/convert/sticker-mix.js
+++ plugins/convert/sticker-mix.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text, args }) => {
     }
     if (!args[0]) throw 'Contoh penggunaan:\n\n*.emojimix 🤨&😣*'
        	let [emoji1, emoji2] = text.split`&`
-    		let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
+    		let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}&${encodeURIComponent(emoji2)}`)).json()
     		for (let res of anu.results) {
     			conn.sendSticker(m.chat, anu.results, m, { packname: set.pack, author: set.auth, asSticker: true})		
     		}
@@ -15,5 +15,7 @@ let handler = async (m, { conn, text, args }) => {
 handler.help = ['emojimix']
 handler.tags = ['sticker']
 handler.command = /^(emojimix)$/i
-handler.limit = true 
+handler.limit = true
+handler.register = true
+
 module.exports = handler

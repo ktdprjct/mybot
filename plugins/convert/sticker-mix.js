b6f89d963b3bf6a38403e1278a4065b3c49c8150
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-06-12 09:35:07 +0800

    update plugins/convert/sticker-mix.js

--- plugins/convert/sticker-mix.js
+++ plugins/convert/sticker-mix.js
@@ -1,26 +1,18 @@
-const fetch = require('node-fetch')
-const { Sticker: createSticker, StickerTypes } = require("wa-sticker-formatter");
+let {fetchJson} = require("../../lib/function")
 
 let handler = async (m, { conn, text, args }) => {
-    if (!args[0]) throw 'Contoh penggunaan:\n\n*.emojimix 🤨.😣*'
-       	let [emoji1, emoji2] = text.split`.`
-    		let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}&${encodeURIComponent(emoji2)}`)).json()
-    		for (let res of anu.results) {
-    			//conn.sendSticker(m.chat, anu.results, m, { packname: set.pack, author: set.auth, asSticker: true})
-    			let sticker = new createSticker(anu.results, {
-                    pack: global.set.pack,
-                    author: global.set.auth,
-                    type: StickerTypes.FULL,
-                    quality: 50,
-                });
-                return conn.reply(await sticker.toMessage());
-    		}
+    if (!args[0]) throw 'Contoh penggunaan:\n\n*.semoji2 🐷&😣*'
+    let [emoji1, emoji2] = text.split`&`
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+    let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
+    for (let res of anu.results) {
+        //conn.sendFile(m.chat, res.url, res.url, "nih", m)
+        conn.sendSticker(m.chat, res.url, m, {author: set.auth, asSticker: /webp/g.test(mime) })
+    }
 }
-		
-handler.help = ['emojimix']
+handler.help = ['semoji2 😂&🥵']
 handler.tags = ['sticker']
-handler.command = /^(s|emoji(mix))$/i
-handler.limit = true
-handler.register = true
+handler.command = /^(emojimix|semoji2|smix)$/i
 
 module.exports = handler

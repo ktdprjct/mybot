d5dbdb2f16e971cfca9969d4546895763b4697aa
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-06-19 09:23:03 +0800

    update main.js

--- main.js
+++ main.js
@@ -28,13 +28,15 @@
     
     global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in set.api.name.s ? set.api.name.s[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: set.api.key.s[name in set.api.name.s ? set.api.name.s[name] : name] } : {}) })) : '')
     global.set.timestamp = { start: new Date }
+    global.db = new Low(new mongoDB('link'))
+    global.DATABASE = global.db
     // global.db = new Low(new mongoDB("url mongodb"))
-    global.db = new Low(
+    /*global.db = new Low(
         /https?:\/\//.test(set.opts['db'] || '') ?
             new cloudDBAdapter(set.opts['db']) : /mongodb/.test(set.opts['db']) ?
             new mongoDB(set.opts['db']) :
             new JSONFile(`${set.opts._[0] ? set.opts._[0] + 'fld' : 'ktdprjct'}.db.json`)
-    ) 
+    )*/
     
     global.loadDatabase = async function loadDatabase() {
         if (global.db.READ) return new Promise((resolve) => setInterval(function () { (!global.db.READ ? (clearInterval(this), resolve(global.db.data == null ? global.loadDatabase() : global.db.data)) : null) }, 1 * 1000))

f50efd2f795862c63f51f8376cc8eff538307d49
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-06-30 16:51:03 +0800

    update plugins/downloader/yt-audio.js

--- plugins/downloader/yt-audio.js
+++ plugins/downloader/yt-audio.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
   if (!args[0]) throw `Link nya mana?`
   m.react('⏱️')
   const link = await fetchJson(`https://xzn.wtf/api/y2mate?url=${args[0]}&apikey=ktdprjct`)
-  let audio = link.links.audio.128kbps.url
+  let audio = link.links.audio["128kbps"].url
   if (!audio) throw `Link download tidak ditemukan`
     conn.sendFile(m.chat, audio, link.title + '.mp3', `Sukses Download Video Dari Link ${args[0]}`, m)
 }

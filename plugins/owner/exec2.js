abde0f780156af372d98cf91296fa815fc7c39ac
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-06-30 14:51:29 +0800

    update plugins/owner/exec2.js

--- plugins/owner/exec2.js
+++ plugins/owner/exec2.js
@@ -27,7 +27,7 @@ let handler = async (m, { conn, isOwner, command, text }) => {
   }
 }
 handler.help = ['$']
-handler.tags = ['advanced']
+handler.tags = ['owner']
 handler.customPrefix = /^[$]/
 handler.command = new RegExp
 handler.rowner = true

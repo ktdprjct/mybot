488391fe6a2c244c426e534df9e45b9ab2e5df30
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-07-01 13:53:48 +0800

    update plugins/downloader/yt-audio.js

--- plugins/downloader/yt-audio.js
+++ plugins/downloader/yt-audio.js
@@ -1,18 +1,32 @@
-let {fetchJson} = require("../../lib/function")
+let { youtubedl, youtubedlv2, youtubedlv3 } = require("@bochilteam/scraper")
+let fetch = require("node-fetch")
 
 let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
-  if (!args[0]) throw `Link nya mana?`
-  m.react('⏱️')
-  const link = await fetchJson(`https://xzn.wtf/api/y2mate?url=${args[0]}&apikey=ktdprjct`)
-  let audio = link.links.audio["128kbps"].url
-  if (!audio) throw `Link download tidak ditemukan`
-    conn.sendFile(m.chat, audio, link.title + '.mp3', `Sukses Download Video Dari Link ${args[0]}`, m)
+    if (!args[0]) throw `Link nya mana?`
+    
+    
+    const { thumbnail, audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
+    let audio, res, link
+    
+    for (let i in _audio) {
+        try {
+            audio = _audio[i]
+            if (isNaN(audio.fileSize)) continue
+            link = await audio.download()
+            if (link) res = await fetch(link)
+            break
+        } catch (e) {
+            console.error(e)
+        }
+    }
+    //console.log('link' + link)
+    conn.sendFile(m.chat, link, "", title, m)
 }
 handler.help = ['ytmp3']
 handler.tags = ['downloader']
 handler.command = /^yt(a|mp3)$/i
 handler.register = true
 handler.limit = true
-handler.disabled = true
+handler.disabled = false
 
 module.exports = handler

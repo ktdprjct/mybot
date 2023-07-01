b99df6dc9b962afaf9b9025d4b9f423ec936be36
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-07-01 15:01:46 +0800

    update plugins/downloader/ig.js

--- plugins/downloader/ig.js
+++ plugins/downloader/ig.js
@@ -13,6 +13,14 @@ let handler = async(m, {conn, text, command}) => {
         case 'igv1': {
             if (!text) return m.reply(`*Masukan URL Instagram nya!*\n\nContoh : .igv1 https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`)
             if (!text.match(/(https:\/\/www.instagram.com)/gi)) return m.reply("ini bukan link ig")
+            //loading
+            const { key } = await conn.reply(m.chat, 'Tunggu sebentar...', m);
+
+            for (let i = 0; i < global.loading.length; i++) {
+                await new Promise(resolve => setTimeout(resolve, 3500));
+                await  conn.editMessage(m.chat, key, global.loading[i], m)
+            }//
+            
             let results = await instagram.v1(text)
             let out = results[0].url
             await m.reply("Permintaan sedang di proses")
@@ -23,6 +31,13 @@ let handler = async(m, {conn, text, command}) => {
         case 'igv2': {
             if (!text) return m.reply(`*Masukan URL Instagram nya!*\n\nContoh : .igv2 https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`)
             if (!text.match(/(https:\/\/www.instagram.com)/gi)) return m.reply("ini bukan link ig")
+            //loading
+            const { key } = await conn.reply(m.chat, 'Tunggu sebentar...', m);
+
+            for (let i = 0; i < global.loading.length; i++) {
+                await new Promise(resolve => setTimeout(resolve, 3500));
+                await  conn.editMessage(m.chat, key, global.loading[i], m)
+            }//
             let anu = await fetchJson(`https://xzn.wtf/api/igdl?url=${text}&apikey=${apikey}`)
             conn.sendFile(m.chat, anu.media[0], "", anu.caption, m)
         }

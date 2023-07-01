2e07760946db1f425bd50bd549f44ad7da3d269d
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-07-01 21:06:32 +0800

    update plugins/internet/chatgpt.js

--- plugins/internet/chatgpt.js
+++ plugins/internet/chatgpt.js
@@ -1,10 +1,20 @@
 let apikey = global.xznkey
+const { ChatGpt } = require('chatgpt-scraper')
 let {fetchJson} = require("../../lib/function")
+
 let handler = async(m, { conn, text }) => {
     if(!text) throw "mau tanya apa?"
-    let hasil = await fetchJson(`https://xzn.wtf/api/openai?text=${text}&apikey=${apikey}`)
-    m.reply(hasil.result)
+    try {
+        let hasil = await fetchJson(`https://xzn.wtf/api/openai?text=${text}&apikey=${apikey}`)
+        m.reply(hasil.result)
+    } catch {
+        try {
+            const res = await ChatGpt(text)
+            m.reply(res)
+        } catch (e) { throw e }
+    }
 }
+
 handler.help = ['ai']
 handler.tags = ['internet']
 handler.command = /^(ai)$/i

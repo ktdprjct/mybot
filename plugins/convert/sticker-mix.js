718af69fcc1032dc4ed63c22e56eb5698c25be61
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-07-01 16:38:01 +0800

    update plugins/convert/sticker-mix.js

--- plugins/convert/sticker-mix.js
+++ plugins/convert/sticker-mix.js
@@ -1,8 +1,9 @@
 let {fetchJson} = require("../../lib/function")
 
 let handler = async (m, { conn, text, args }) => {
-    if (!args[0]) throw 'Contoh penggunaan:\n\n*.semoji2 🐷&😣*'
-    let [emoji1, emoji2] = text.split`&`
+    if (!args[0] || !args[2]) throw 'Contoh penggunaan:\n\n*.semoji2 🐷.😣*'
+    let [emoji1, emoji2] = text.split`.`
+    
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
     let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)

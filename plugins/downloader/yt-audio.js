f09f48bc3bce70d09d71a031306d3350162b0441
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-07-01 16:10:56 +0800

    update plugins/downloader/yt-audio.js

--- plugins/downloader/yt-audio.js
+++ plugins/downloader/yt-audio.js
@@ -3,7 +3,13 @@ let fetch = require("node-fetch")
 
 let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
     if (!args[0]) throw `Link nya mana?`
-    
+    //loading
+    const { key } = await conn.reply(m.chat, 'Tunggu sebentar...', m);
+
+    for (let i = 0; i < global.loading.length; i++) {
+        await new Promise(resolve => setTimeout(resolve, 3500));
+        await  conn.editMessage(m.chat, key, global.loading[i], m)
+    }//
     
     const { thumbnail, audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
     let audio, res, link

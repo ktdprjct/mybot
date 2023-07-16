6c75f16fb29efca58795ef4c145f96416b1b4b6d
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-07-17 01:22:48 +0800

    update plugins/regist-db/verifikasi.js

--- plugins/regist-db/verifikasi.js
+++ plugins/regist-db/verifikasi.js
@@ -3,17 +3,21 @@ let handler = async(m, { conn, text }) => {
     let id = m.chat
     let user = global.db.data.users[m.sender]
     conn.sendMail = conn.sendMail ? conn.sendMail : {}
+    let ingfo = `「 *+add regist user!* 」\n\n👤 NAME : ${conn.sendMail[id].id}💌 EMAIL : ${conn.sendMail[id].email}\n✨ STATUS  : Sukses`
     
     if(conn.sendMail[id] == undefined ) {
         conn.reply(m.chat, "waktu habis. silahkan regist ulang!", m)
     } else {
         if (text == conn.sendMail[id].otpCode) {
-        conn.reply(m.chat, "berhasil", m)
+        conn.reply(m.chat, `sekarang kamu terdaftar di database bot dengan email ${conn.sendMail[id].email}`, m)
         user.email = conn.sendMail[id].email
         user.regTime = + new Date
         user.registered = true
+        
+        conn.reply(set.owner[0][0] + `@s.whatsapp.net`, ingfo, m)
+        
         delete conn.sendMail[id]
-        } else conn.reply(m.chat, "gagal silahkan masukkan ulang codenya", m)
+        } else conn.reply(m.chat, "otp yang kamu masukkan salah, silahkan masukkan ulang codenya", m)
     }
 }
 handler.help = ['otp']

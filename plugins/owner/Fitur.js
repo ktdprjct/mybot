6836933ccadef360cb4891cc45ce4744d28a6a8a
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-07-19 00:48:46 +0800

    update plugins/owner/Fitur.js

--- plugins/owner/Fitur.js
+++ plugins/owner/Fitur.js
@@ -3,14 +3,33 @@ let fs = require("fs")
 let handler = async(m, { conn, text }) => {
     command = command.toLowerCase()
     switch (command) {
-        case 'sf':
-            if (!text) throw `where is the path?\n\nexample:\n${usedPrefix + command} plugins/menu.js`
+        
+        case 'sf': {
+            if (!text) throw `where is the path?\n\nexample:\n.sf plugins/main-info/menu.js`
             if (!m.quoted.text) throw `reply code`
             
             let path = `${text}`
             await fs.writeFileSync(path, m.quoted.text)
             m.reply(`Saved ${path} to file!`)
+        }
             break;
+        
+        case 'delfitur': {
+            if (!text) throw `where is the path?\n\nexample:\n.delfitur plugins/main-info/menu.js`
+            
+            await fs.unlinkSync(text)
+            m.reply(`Delete ${path} to file!`)
+        }
+            break;
+        
+        case 'openfile': {
+            if (!text) throw `where is the path?\n\nexample:\n.openfile plugins/main-info/menu.js`
+            
+            let pile = await fs.readFileSync(text)
+            await conn.sendFile(m.chat, pile, '', 'Nihh,?', m)
+        }
+            break;
+        
         
     }
 }

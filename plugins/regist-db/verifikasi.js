e5eb2f2a0223fd592d8fef810d648ca275855e97
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-07-19 12:11:04 +0800

    update plugins/regist-db/verifikasi.js

--- plugins/regist-db/verifikasi.js
+++ plugins/regist-db/verifikasi.js
@@ -1,23 +1,25 @@
 let handler = async(m, { conn, text }) => {
-    let user = global.db.data.users[m.sender]
-    if(user.registered === true) throw "kamu sudah terdaftar di database bot"
-    if(!text) throw "masukkan code otpnya!!!"
     let id = m.chat
+    let user = global.db.data.users[m.sender]
     conn.sendMail = conn.sendMail ? conn.sendMail : {}
-    let ingfo = `「 *+add regist user!* 」\n\n👤 NAME : ${conn.getName(conn.sendMail[id].id)}💌 EMAIL : ${conn.sendMail[id].email}\n✨ STATUS  : Sukses`
+    if(user.registered === true) throw "kamu sudah terdaftar di database bot"
+    
     
     if(conn.sendMail[id] == undefined ) {
-        conn.reply(m.chat, "waktu habis. silahkan regist ulang!", m)
+        conn.reply(m.chat, "silahkan regist dengan cara : .reg <emailmu>", m)
     } else {
+        if(!text) throw "masukkan code otpnya!!!"
+        
         if (text == conn.sendMail[id].otpCode) {
-        conn.reply(m.chat, `sekarang kamu terdaftar di database bot dengan email ${conn.sendMail[id].email}`, m)
-        user.email = conn.sendMail[id].email
-        user.regTime = + new Date
-        user.registered = true
-        
-        await conn.reply(set.owner[0][0] + `@s.whatsapp.net`, ingfo, m)
-        
-        delete conn.sendMail[id]
+            conn.reply(m.chat, `sekarang kamu terdaftar di database bot dengan email ${conn.sendMail[id].email}`, m)
+            user.email = conn.sendMail[id].email
+            user.regTime = + new Date
+            user.registered = true
+            
+            let ingfo = `「 *+add regist user!* 」\n\n👤 NAME : ${conn.getName(conn.sendMail[id].id)}\n💌 EMAIL : ${conn.sendMail[id].email}\n✨ STATUS  : Sukses`
+            await conn.reply(set.owner[0][0] + `@s.whatsapp.net`, ingfo, m)
+            
+            delete conn.sendMail[id]
         } else conn.reply(m.chat, "otp yang kamu masukkan salah, silahkan masukkan ulang codenya", m)
     }
 }

8bca334b2d6fe32c73b651ab39aae49b2d435bf1
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-07-26 14:18:42 +0800

    update plugins/tools/carbon.js

--- plugins/tools/carbon.js
+++ plugins/tools/carbon.js
@@ -1,11 +1,10 @@
 let Carbon = require("unofficial-carbon-now")
 
 let handler = async(m, { conn, usedPrefix, command, text }) => {
-  if (!m.quoted && !text) throw `${set.sb} *Example* : ${usedPrefix + command} text/reply\n\n_Gesek pesan ini kekanan untuk membuat gambar teks *carbon*_`             
-  m.react('⏱️')
-  let buat = new Carbon.createCarbon().setCode(m.quoted ? m.quoted.text : text)
-  let hasil = await Carbon.generateCarbon(buat)
-  conn.sendMedia(m.chat, hasil, m)
+    if (!m.quoted && !text) throw `${set.sb} *Example* : ${usedPrefix + command} text/reply\n\n_Gesek pesan ini kekanan untuk membuat gambar teks *carbon*_`
+    let buat = new Carbon.createCarbon().setCode(m.quoted ? m.quoted.text : text)
+    let hasil = await Carbon.generateCarbon(buat)
+    conn.sendMedia(m.chat, hasil, m)
 }
 handler.help = ['carbon'].map(v => v + ' <text>')
 handler.tags = ['tools']

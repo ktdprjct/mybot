510f5ec6244f3381c9a8acd1d72a8318fbb45e05
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-11-11 21:58:47 +0800

    + confirmasi

--- plugins/downloader/yt.js
+++ plugins/downloader/yt.js
@@ -1,6 +1,8 @@
 let fetch = require("node-fetch")
-const yts = require("youtube-yts")
-let handler = async(m, {conn, text, command, args, usedPrefix}) => {
+let yts = require("youtube-yts")
+let confirmation = {}
+
+async function handler(m, {conn, text, command, args, usedPrefix}) {
     if(!text) throw conn.sendMessage(m.chat, `Contoh: ${usedPrefix + command} judul atau url`)
     
     let link = await yts(args[0])
@@ -11,22 +13,52 @@ let handler = async(m, {conn, text, command, args, usedPrefix}) => {
 ➥ ᴀᴜᴛʜᴏʀ: ${vid.author.name}
 ➥ ᴅᴜʀᴀᴛɪᴏɴ: ${vid.timestamp}
 ➥ ᴜᴘʟᴏᴀᴅ ᴏɴ: ${vid.ago}
-➥ ᴜʀʟ: ${vid.url}`
+➥ ᴜʀʟ: ${vid.url}
+
+_Untuk melanjutkan silahkan ketik_
+*1/Y/Ya = Download Audio*
+*0/N/No = Untuk Membatalkan*
+`
 
     conn.sendMessage(m.chat, {image: {url: vid.thumbnail}, caption: texts})
     
-    let ytdl = await (await fetch(`https://skizo.tech/api/yt1s?url=${vid.url}&apikey=filand`)).json()
-    let res_m = await ytdl.audio['128k'].url
-    
-    await conn.sendMessage(
-        m.chat, 
-        { audio: { url: res_m }, mimetype: 'audio/mp4' },
-        { url: res_m } // can send mp3, mp4, & ogg
-    )
-
+    confirmation[m.sender] = {
+        sender: m.sender,
+        message: m,
+        link: vid.url,
+        timeout: setTimeout(() => (m.reply('Timeout'), delete confirmation[m.sender]), 60 * 1000)
+    }
 }
+
+handler.before = async function(m, { conn }) {
+    if (m.isBaileys) return
+    if (!(m.sender in confirmation)) return
+    if (!m.text) return
+    let { timeout, sender, message, link} = confirmation[m.sender]
+    if (m.id === message.id) return
+    //commandnya
+    if (/(0|n(o)?)/g.test(m.text.toLowerCase())) {
+        clearTimeout(timeout)
+        delete confirmation[sender]
+        return m.reply('Reject')
+    }
+    if (/(1|y(a)?)/g.test(m.text.toLowerCase())) {
+        m.reply('wait...')
+        let ytdl = await (await fetch(`https://skizo.tech/api/yt1s?url=${link}&apikey=filand`)).json()
+        let res_m = await ytdl.audio['128k'].url
+        
+        await conn.sendMessage(
+            m.chat, 
+            { audio: { url: res_m }, mimetype: 'audio/mp4' },
+            { url: res_m } // can send mp3, mp4, & ogg
+        )
+        clearTimeout(timeout)
+        delete confirmation[sender]
+    }
+}
+
+
 handler.help = ['play'].map(v => v + ' <query>')
 handler.tags = ['downloader']
 handler.command = /^play$/i
 module.exports = handler
-

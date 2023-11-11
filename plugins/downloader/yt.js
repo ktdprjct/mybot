26d9f96345cc6e2cefb0b49532615ed8586373eb
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2023-11-11 19:06:33 +0800

    update plugins/downloader/yt.js

--- plugins/downloader/yt.js
+++ plugins/downloader/yt.js
@@ -1,1 +1,30 @@
+let fetch = require("node-fetch")
+let { youtubeSearch, youtubedl, youtubedlv2 } = require('@bochilteam/scraper')
+let handler = async(m, {conn, text, command, args, usedPrefix}) => {
+    if(!text) throw conn.sendMessage(m.chat, `Contoh: ${usedPrefix + command} judul atau url`)
+    
+    let Tot = await youtubeSearch(args[0])
+    let vid = Tot.video[0]
+    let texts = `
+Judul: ${vid.title}
+desc: ${vid.description}
+Durasi: ${vid.durationH}
+Publish: ${vid.publishedTime}`
+
+    conn.sendMessage(m.chat, {image: {url: vid.thumbnail}, caption: texts})
+    
+    let ytdl = await (await fetch(`https://skizo.tech/api/yt1s?url=${vid.url}&apikey=filand`)).json()
+    let res_m = await ytdl.audio['128k'].url
+    
+    await conn.sendMessage(
+        m.chat, 
+        { audio: { url: res_m }, mimetype: 'audio/mp4' },
+        { url: res_m } // can send mp3, mp4, & ogg
+    )
+
+}
+handler.help = ['play'].map(v => v + ' <query>')
+handler.tags = ['downloader']
+handler.command = /^play$/i
+module.exports = handler
 
